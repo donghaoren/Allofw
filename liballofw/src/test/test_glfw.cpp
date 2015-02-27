ef282fbea6d97c82213e0357452b61fa0d631f91
Donghao Ren <donghao.ren@gmail.com>
2015-02-26 22:25:07 -0800

    New test form the internet.

--- liballofw/src/test/test_glfw.cpp
+++ liballofw/src/test/test_glfw.cpp
@@ -1,9 +1,436 @@
-#include <GLFW/glfw3.h>
 #include "opengl.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <math.h>
 
 using namespace allofw;
 
 POpenGLWindow w;
+
+// Data for drawing Axis
+float verticesAxis[] = {-20.0f, 0.0f, 0.0f, 1.0f,
+            20.0f, 0.0f, 0.0f, 1.0f,
+
+            0.0f, -20.0f, 0.0f, 1.0f,
+            0.0f,  20.0f, 0.0f, 1.0f,
+
+            0.0f, 0.0f, -20.0f, 1.0f,
+            0.0f, 0.0f,  20.0f, 1.0f};
+
+float colorAxis[] = {   0.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 0.0f};
+
+// Data for triangle 1
+float vertices1[] = {   -3.0f, 0.0f, -5.0f, 1.0f,
+            -1.0f, 0.0f, -5.0f, 1.0f,
+            -2.0f, 2.0f, -5.0f, 1.0f};
+
+float colors1[] = { 0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f,0.0f, 1.0f, 1.0f};
+
+// Data for triangle 2
+float vertices2[] = {   1.0f, 0.0f, -5.0f, 1.0f,
+            3.0f, 0.0f, -5.0f, 1.0f,
+            2.0f, 2.0f, -5.0f, 1.0f};
+
+float colors2[] = { 1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f,0.0f, 0.0f, 1.0f};
+
+#define MULTI_LINE_STRING(...) #__VA_ARGS__
+// Shader Names
+const char *vertexFileName = "#version 150\n" MULTI_LINE_STRING(
+    uniform mat4 viewMatrix, projMatrix;
+
+    in vec4 position;
+    in vec3 color;
+
+    out vec3 Color;
+
+    void main()
+    {
+        Color = color;
+        gl_Position = projMatrix * viewMatrix * position ;
+    }
+);
+const char *fragmentFileName = "#version 150\n" MULTI_LINE_STRING(
+    in vec3 Color;
+    out vec4 outputF;
+
+    void main()
+    {
+        outputF = vec4(Color,1.0);
+    }
+);
+
+// Program and Shader Identifiers
+GLuint p,v,f;
+
+// Vertex Attribute Locations
+GLuint vertexLoc, colorLoc;
+
+// Uniform variable Locations
+GLuint projMatrixLoc, viewMatrixLoc;
+
+// Vertex Array Objects Identifiers
+GLuint vao[3];
+
+// storage for Matrices
+float projMatrix[16];
+float viewMatrix[16];
+
+// ----------------------------------------------------
+// VECTOR STUFF
+//
+
+// res = a cross b;
+void crossProduct( float *a, float *b, float *res) {
+
+    res[0] = a[1] * b[2]  -  b[1] * a[2];
+    res[1] = a[2] * b[0]  -  b[2] * a[0];
+    res[2] = a[0] * b[1]  -  b[0] * a[1];
+}
+
+// Normalize a vec3
+void normalize(float *a) {
+
+    float mag = sqrt(a[0] * a[0]  +  a[1] * a[1]  +  a[2] * a[2]);
+
+    a[0] /= mag;
+    a[1] /= mag;
+    a[2] /= mag;
+}
+
+// ----------------------------------------------------
+// MATRIX STUFF
+//
+
+// sets the square matrix mat to the identity matrix,
+// size refers to the number of rows (or columns)
+void setIdentityMatrix( float *mat, int size) {
+
+    // fill matrix with 0s
+    for (int i = 0; i < size * size; ++i)
+            mat[i] = 0.0f;
+
+    // fill diagonal with 1s
+    for (int i = 0; i < size; ++i)
+        mat[i + i * size] = 1.0f;
+}
+
+//
+// a = a * b;
+//
+void multMatrix(float *a, float *b) {
+
+    float res[16];
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            res[j*4 + i] = 0.0f;
+            for (int k = 0; k < 4; ++k) {
+                res[j*4 + i] += a[k*4 + i] * b[j*4 + k];
+            }
+        }
+    }
+    memcpy(a, res, 16 * sizeof(float));
+
+}
+
+// Defines a transformation matrix mat with a translation
+void setTranslationMatrix(float *mat, float x, float y, float z) {
+
+    setIdentityMatrix(mat,4);
+    mat[12] = x;
+    mat[13] = y;
+    mat[14] = z;
+}
+
+// ----------------------------------------------------
+// Projection Matrix
+//
+
+void buildProjectionMatrix(float fov, float ratio, float nearP, float farP) {
+
+    float f = 1.0f / tan (fov * (M_PI / 360.0));
+
+    setIdentityMatrix(projMatrix,4);
+
+    projMatrix[0] = f / ratio;
+    projMatrix[1 * 4 + 1] = f;
+    projMatrix[2 * 4 + 2] = (farP + nearP) / (nearP - farP);
+    projMatrix[3 * 4 + 2] = (2.0f * farP * nearP) / (nearP - farP);
+    projMatrix[2 * 4 + 3] = -1.0f;
+    projMatrix[3 * 4 + 3] = 0.0f;
+}
+
+// ----------------------------------------------------
+// View Matrix
+//
+// note: it assumes the camera is not tilted,
+// i.e. a vertical up vector (remmeber gluLookAt?)
+//
+
+void setCamera(float posX, float posY, float posZ,
+               float lookAtX, float lookAtY, float lookAtZ) {
+
+    float dir[3], right[3], up[3];
+
+    up[0] = 0.0f;   up[1] = 1.0f;   up[2] = 0.0f;
+
+    dir[0] =  (lookAtX - posX);
+    dir[1] =  (lookAtY - posY);
+    dir[2] =  (lookAtZ - posZ);
+    normalize(dir);
+
+    crossProduct(dir,up,right);
+    normalize(right);
+
+    crossProduct(right,dir,up);
+    normalize(up);
+
+    float aux[16];
+
+    viewMatrix[0]  = right[0];
+    viewMatrix[4]  = right[1];
+    viewMatrix[8]  = right[2];
+    viewMatrix[12] = 0.0f;
+
+    viewMatrix[1]  = up[0];
+    viewMatrix[5]  = up[1];
+    viewMatrix[9]  = up[2];
+    viewMatrix[13] = 0.0f;
+
+    viewMatrix[2]  = -dir[0];
+    viewMatrix[6]  = -dir[1];
+    viewMatrix[10] = -dir[2];
+    viewMatrix[14] =  0.0f;
+
+    viewMatrix[3]  = 0.0f;
+    viewMatrix[7]  = 0.0f;
+    viewMatrix[11] = 0.0f;
+    viewMatrix[15] = 1.0f;
+
+    setTranslationMatrix(aux, -posX, -posY, -posZ);
+
+    multMatrix(viewMatrix, aux);
+}
+
+// ----------------------------------------------------
+
+void changeSize(int w, int h) {
+
+    float ratio;
+    // Prevent a divide by zero, when window is too short
+    // (you cant make a window of zero width).
+    if(h == 0)
+        h = 1;
+
+    // Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+    ratio = (1.0f * w) / h;
+    buildProjectionMatrix(53.13f, ratio, 1.0f, 30.0f);
+}
+
+void setupBuffers() {
+
+    GLuint buffers[2];
+
+    glGenVertexArrays(3, vao);
+    //
+    // VAO for first triangle
+    //
+    glBindVertexArray(vao[0]);
+    // Generate two slots for the vertex and color buffers
+    glGenBuffers(2, buffers);
+    // bind buffer for vertices and copy data into buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(vertexLoc);
+    glVertexAttribPointer(vertexLoc, 4, GL_FLOAT, 0, 0, 0);
+
+    // bind buffer for colors and copy data into buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors1), colors1, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(colorLoc);
+    glVertexAttribPointer(colorLoc, 4, GL_FLOAT, 0, 0, 0);
+
+    //
+    // VAO for second triangle
+    //
+    glBindVertexArray(vao[1]);
+    // Generate two slots for the vertex and color buffers
+    glGenBuffers(2, buffers);
+
+    // bind buffer for vertices and copy data into buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(vertexLoc);
+    glVertexAttribPointer(vertexLoc, 4, GL_FLOAT, 0, 0, 0);
+
+    // bind buffer for colors and copy data into buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors2), colors2, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(colorLoc);
+    glVertexAttribPointer(colorLoc, 4, GL_FLOAT, 0, 0, 0);
+
+    //
+    // This VAO is for the Axis
+    //
+    glBindVertexArray(vao[2]);
+    // Generate two slots for the vertex and color buffers
+    glGenBuffers(2, buffers);
+    // bind buffer for vertices and copy data into buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesAxis), verticesAxis, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(vertexLoc);
+    glVertexAttribPointer(vertexLoc, 4, GL_FLOAT, 0, 0, 0);
+
+    // bind buffer for colors and copy data into buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colorAxis), colorAxis, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(colorLoc);
+    glVertexAttribPointer(colorLoc, 4, GL_FLOAT, 0, 0, 0);
+
+}
+
+void setUniforms() {
+
+    // must be called after glUseProgram
+    glUniformMatrix4fv(projMatrixLoc,  1, false, projMatrix);
+    glUniformMatrix4fv(viewMatrixLoc,  1, false, viewMatrix);
+}
+
+void renderScene(void) {
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    setCamera(10,2,10,0,2,-5);
+
+    glUseProgram(p);
+    setUniforms();
+
+    glBindVertexArray(vao[0]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glBindVertexArray(vao[1]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glBindVertexArray(vao[2]);
+    glDrawArrays(GL_LINES, 0, 6);
+}
+
+void processNormalKeys(unsigned char key, int x, int y) {
+
+    if (key == 27) {
+        glDeleteVertexArrays(3,vao);
+        glDeleteProgram(p);
+        glDeleteShader(v);
+        glDeleteShader(f);
+        exit(0);
+    }
+}
+
+#define printOpenGLError() printOglError(__FILE__, __LINE__)
+
+int printOglError(char *file, int line)
+{
+    //
+    // Returns 1 if an OpenGL error occurred, 0 otherwise.
+    //
+    GLenum glErr;
+    int    retCode = 0;
+
+    glErr = glGetError();
+    while (glErr != GL_NO_ERROR)
+    {
+        //printf("glError in file %s @ line %d: %s\n", file, line, gluErrorString(glErr));
+        retCode = 1;
+        glErr = glGetError();
+    }
+    return retCode;
+}
+
+void printShaderInfoLog(GLuint obj)
+{
+    int infologLength = 0;
+    int charsWritten  = 0;
+    char *infoLog;
+
+    glGetShaderiv(obj, GL_INFO_LOG_LENGTH,&infologLength);
+
+    if (infologLength > 0)
+    {
+        infoLog = (char *)malloc(infologLength);
+        glGetShaderInfoLog(obj, infologLength, &charsWritten, infoLog);
+        printf("%s\n",infoLog);
+        free(infoLog);
+    }
+}
+
+void printProgramInfoLog(GLuint obj)
+{
+    int infologLength = 0;
+    int charsWritten  = 0;
+    char *infoLog;
+
+    glGetProgramiv(obj, GL_INFO_LOG_LENGTH,&infologLength);
+
+    if (infologLength > 0)
+    {
+        infoLog = (char *)malloc(infologLength);
+        glGetProgramInfoLog(obj, infologLength, &charsWritten, infoLog);
+        printf("%s\n",infoLog);
+        free(infoLog);
+    }
+}
+
+GLuint setupShaders() {
+
+    char *vs = NULL,*fs = NULL,*fs2 = NULL;
+
+    GLuint p,v,f;
+
+    v = glCreateShader(GL_VERTEX_SHADER);
+    f = glCreateShader(GL_FRAGMENT_SHADER);
+
+    const char * vv = vertexFileName;
+    const char * ff = fragmentFileName;
+
+    glShaderSource(v, 1, &vv,NULL);
+    glShaderSource(f, 1, &ff,NULL);
+
+
+
+    glCompileShader(v);
+    glCompileShader(f);
+
+    printShaderInfoLog(v);
+    printShaderInfoLog(f);
+
+    p = glCreateProgram();
+    glAttachShader(p,v);
+    glAttachShader(p,f);
+
+    glBindFragDataLocation(p, 0, "outputF");
+    glLinkProgram(p);
+    printProgramInfoLog(p);
+
+    vertexLoc = glGetAttribLocation(p,"position");
+    colorLoc = glGetAttribLocation(p, "color");
+
+    projMatrixLoc = glGetUniformLocation(p, "projMatrix");
+    viewMatrixLoc = glGetUniformLocation(p, "viewMatrix");
+
+    return(p);
+}
 
 class MyDelegate : public OpenGLWindow::Delegate {
 public:
@@ -17,11 +444,9 @@ public:
         printf("onClose\n");
     }
     virtual void onRefresh() {
-        glClearColor(0.1, 0.3, 0.5, 1);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glFlush();
-        int val = GL_GEOMETRY_SHADER;
-        printf("%d\n", val);
+        Size2i sz = w->getFramebufferSize();
+        changeSize(sz.x, sz.y);
+        renderScene();
         w->swapBuffers();
         printf("onRefresh\n");
     }
@@ -50,9 +475,20 @@ int main(void)
 
     printf("OpenGL: %s\n", glGetString(GL_VERSION));
 
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    p = setupShaders();
+    setupBuffers();
+
     MyDelegate d;
     w->setDelegate(&d);
     while(1) {
+        Size2i sz = w->getFramebufferSize();
+        changeSize(sz.x, sz.y);
+        renderScene();
+        w->swapBuffers();
+        int err = glGetError();
+        printf("%d\n", err);
         w->pollEvents();
     }
 }
