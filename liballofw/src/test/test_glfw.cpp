c19ab29f0507e9681e136f7182236fe08ce47042
Donghao Ren <donghao.ren@gmail.com>
2015-02-26 17:52:27 -0800

    GLFW working.

--- liballofw/src/test/test_glfw.cpp
+++ liballofw/src/test/test_glfw.cpp
@@ -32,7 +32,10 @@ int main(void)
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    glfwWindowHint(GLFW_STEREO, GL_TRUE);
+    glfwWindowHint(GLFW_DECORATED, GL_FALSE);
+
+    window = glfwCreateWindow(1920 * 2, 1200, "Simple example", NULL, NULL);
     showMonitors();
     if (!window)
     {
@@ -44,9 +47,11 @@ int main(void)
     glfwSetKeyCallback(window, key_callback);
     while (!glfwWindowShouldClose(window))
     {
+        glDrawBuffer(GL_BACK_LEFT);
         float ratio;
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
+        printf("%d %d\n", width, height);
         ratio = width / (float) height;
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -58,11 +63,11 @@ int main(void)
         glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
         glBegin(GL_TRIANGLES);
         glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
+        glVertex3f(-1.6f, -1.4f, 1.f);
         glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
+        glVertex3f(1.6f, -1.4f, 0.f);
         glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
+        glVertex3f(0.f, 1.6f, 0.f);
         glEnd();
         glfwSwapBuffers(window);
         glfwPollEvents();
