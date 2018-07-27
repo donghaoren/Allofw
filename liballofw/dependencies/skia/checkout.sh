53d98ab2d3ed6dfa96f897db580fdd1e90ca0378
Donghao Ren <donghao.ren@gmail.com>
2018-07-26 22:42:13 -0700

    update skia build

--- liballofw/dependencies/skia/checkout.sh
+++ liballofw/dependencies/skia/checkout.sh
@@ -4,4 +4,4 @@ git clone 'https://chromium.googlesource.com/chromium/tools/depot_tools.git'
 git clone 'https://skia.googlesource.com/skia'
 
 cd skia
-git checkout chrome/m57
+git checkout chrome/m69
