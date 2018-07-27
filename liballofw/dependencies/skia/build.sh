0bb6b2abdd3066912e7c87a7b92779d642d8b6d4
Donghao Ren <donghao.ren@gmail.com>
2018-07-26 22:59:59 -0700

    fix skia build

--- liballofw/dependencies/skia/build.sh
+++ liballofw/dependencies/skia/build.sh
@@ -4,5 +4,5 @@ export PATH="${PWD}/depot_tools:${PATH}"
 cp SkUserConfig.h skia/include/config
 cd skia
 python tools/git-sync-deps
-bin/gn gen out/Release  --args='cc="clang" cxx="clang++" is_official_build=true is_component_build=true is_debug=false extra_cflags_cc=["-frtti"]'
+bin/gn gen out/Release  --args='cc="clang" cxx="clang++" is_official_build=true is_component_build=true skia_use_system_expat=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_system_icu=false is_debug=false extra_cflags_cc=["-frtti"]'
 ninja -C out/Release
